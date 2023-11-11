dda216d9b830695137afe404ea5e207d64b5f3a6
kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-11 15:28:22 +0530

    change the table columns

--- video_watermaker_server/database/models.py
+++ video_watermaker_server/database/models.py
@@ -7,15 +7,14 @@ Base = declarative_base()
 
 class UploadData(Base):
     __tablename__ = "upload_data"
-    uid = Column(String(50), primary_key=True, default=str(uuid.uuid4()), unique=True)
-    filename = Column(String(500))
+    uid = Column(String(50), primary_key=True, unique=True)
+    uploadBy = Column(String(500))
     uploadTime = Column(DateTime, default=func.current_timestamp())
 
 
 class ProcessedData(Base):
     __tablename__ = "processed_data"
-    uid = uid = Column(
-        String(50), primary_key=True, default=str(uuid.uuid4()), unique=True
+    uid = Column(
+        String(50), primary_key=True, unique=True
     )
-    filename = Column(String(500))
-    uploadTime = Column(DateTime, default=func.current_timestamp())
+    ProceesedTime = Column(DateTime, default=func.current_timestamp())
